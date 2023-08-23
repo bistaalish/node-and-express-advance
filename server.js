fc9344182686538d46edb42ea607e49d00a00b46
Alish Bista <bistace321@gmail.com>
2023-08-23 21:13:18 +0545

    fix: hashing your password

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -53,8 +54,7 @@ myDB(async client => {
     res.redirect('/');
   });
 
-  app.route('/register')
-  .post((req, res, next) => {
+  app.route('/register').post((req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, 12);
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
       if (err) {
@@ -96,7 +96,9 @@ myDB(async client => {
       console.log(`User ${username} attempted to log in.`);
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
+      if (!bcrypt.compareSync(password, user.password)) { 
+          return done(null, false);
+      }
       return done(null, user);
     });
   }));

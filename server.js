9f62b90e7892ad086c97d231ad4dfb48d6516aa8
Alish Bista <bistace321@gmail.com>
2023-08-23 19:05:56 +0545

    feat: serialization of a User Object

--- server.js
+++ server.js
@@ -30,6 +30,15 @@ app.route('/').get((req, res) => {
   res.render('index',{title:'Hello', message: 'Please log in'})
 });
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  // });
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);

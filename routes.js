c6f7d6291f49b528368ead876bbf43320421bdcf
Alish Bista <bistace321@gmail.com>
2023-08-23 21:24:42 +0545

    feat: Implementation of Social Authentication

--- routes.js
+++ routes.js
@@ -55,6 +55,11 @@ module.exports = function (app, myDataBase) {
     }
   );
 
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  })
+  
   app.use((req, res, next) => {
     res.status(404)
       .type('text')
